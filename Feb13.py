f72692998f68d0e03973ef0648d8d749da1bd47d
ScarlettF572 <scarlettf572@stemcollegiate.ca>
2025-02-13 09:24:19 -0700

    Create Feb13.py

---
+++ Feb13.py
@@ -0,0 +1,5 @@
+# for day 1 I learned how to create a comment
+print("Hello Replit")
+print("87")
+print("Travis Kelce")
+print("""I am signing up for Replit's 100 days of Python challenge!I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
